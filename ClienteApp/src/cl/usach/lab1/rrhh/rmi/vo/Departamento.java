288d272dd2a7b00c9f44310cb39361f560fa7664
Rodrigo Araya C. <rodrigo.araya.c@usach.cl>
2015-04-18 22:07:47 -0300

    Agregar vo Departamento

---
+++ ClienteApp/src/cl/usach/lab1/rrhh/rmi/vo/Departamento.java
@@ -0,0 +1,43 @@
+package cl.usach.lab1.rrhh.rmi.vo;
+
+import java.io.Serializable;
+
+public class Departamento implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2509802864598818760L;
+	private int id_departamento;
+	private String nombre_departamento;
+	private String administrador;
+	private String ubicacion;
+	public int getId_departamento() {
+		return id_departamento;
+	}
+	public void setId_departamento(int id_departamento) {
+		this.id_departamento = id_departamento;
+	}
+	public String getNombre_departamento() {
+		return nombre_departamento;
+	}
+	public void setNombre_departamento(String nombre_departamento) {
+		this.nombre_departamento = nombre_departamento;
+	}
+	public String getAdministrador() {
+		return administrador;
+	}
+	public void setAdministrador(String administrador) {
+		this.administrador = administrador;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
+
+
+}
